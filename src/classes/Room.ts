f3010707c233a1e295c6fc819b9beaafc7916f07
Gabriel Ávila <avilabiel@gmail.com>
2022-01-24 01:15:49 -0300

    so now we fixed the winner of each room

--- src/classes/Room.ts
+++ src/classes/Room.ts
@@ -48,18 +48,18 @@ export default class Room {
 
   private isPlayerWinner(player: Player): boolean {
     const didPlayerWinVertical =
-      player.movements.every((movement) => [1, 4, 7].includes(movement)) ||
-      player.movements.every((movement) => [2, 5, 8].includes(movement)) ||
-      player.movements.every((movement) => [3, 6, 9].includes(movement));
+      [1, 4, 7].every((movement) => player.movements.includes(movement)) ||
+      [2, 5, 8].every((movement) => player.movements.includes(movement)) ||
+      [3, 6, 9].every((movement) => player.movements.includes(movement));
 
     const didPlayerWinHorizontal =
-      player.movements.every((movement) => [1, 2, 3].includes(movement)) ||
-      player.movements.every((movement) => [4, 5, 6].includes(movement)) ||
-      player.movements.every((movement) => [7, 8, 9].includes(movement));
+      [1, 2, 3].every((movement) => player.movements.includes(movement)) ||
+      [4, 5, 6].every((movement) => player.movements.includes(movement)) ||
+      [7, 8, 9].every((movement) => player.movements.includes(movement));
 
     const didPlayerWinDiagonal =
-      player.movements.every((movement) => [1, 5, 9].includes(movement)) ||
-      player.movements.every((movement) => [3, 5, 7].includes(movement));
+      [1, 5, 9].every((movement) => player.movements.includes(movement)) ||
+      [3, 5, 7].every((movement) => player.movements.includes(movement));
 
     const didPlayerWin =
       didPlayerWinVertical || didPlayerWinHorizontal || didPlayerWinDiagonal;
